ad760926b744ebf7c3b537d9abbf6958e6555a51
Nahary Toavina <toavina.rabenjanaharisoa@gmail.com>
2025-11-09 18:05:31 +0300

    Afficher hello world

---
+++ model/model.py
@@ -0,0 +1,8 @@
+class Model:
+    """Modèle : contient les données de l'application."""
+
+    def __init__(self):
+        self.message = "Hello world"
+
+    def get_message(self):
+        return self.message
